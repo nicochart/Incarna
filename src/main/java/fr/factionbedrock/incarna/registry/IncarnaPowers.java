f7fdf1f2275afbf04654bf1ea26a366e9c7de337
Nicolas Hochart <nicolas@freddom.fr>
2025-05-27 23:51:14 +0200

    [Powers, Species] Trying to smooth level effect on powers

--- src/main/java/fr/factionbedrock/incarna/registry/IncarnaPowers.java
+++ src/main/java/fr/factionbedrock/incarna/registry/IncarnaPowers.java
@@ -20,21 +20,33 @@ public class IncarnaPowers
     public static IncarnaPower PREVENT_BOW_USE = new ItemUseRestrictionPower((stack) -> {return !stack.isOf(Items.BOW);});
     public static IncarnaPower CONSTANT_FIRE_RESISTANCE_EFFECT = new ConstantStatusEffectPower(StatusEffects.FIRE_RESISTANCE);
     public static IncarnaPower CONSTANT_SLOW_FALLING_EFFECT = new ConstantStatusEffectPower(StatusEffects.SLOW_FALLING);
-    public static IncarnaPower CONSTANT_SPEED_EFFECT = new ConstantStatusEffectPower(StatusEffects.SPEED, (level) -> Math.max(0, level - 1));
+    public static IncarnaPower CONSTANT_SPEED_EFFECT = new ConstantStatusEffectPower(StatusEffects.SPEED, IncarnaPowers::levelScaler_0_2);
     public static IncarnaPower CONSTANT_NIGHT_VISION_EFFECT = new ConstantStatusEffectPower(StatusEffects.NIGHT_VISION);
-    public static IncarnaPower CONSTANT_SPEED_MODIFIER = new AttributeModifierPower(EntityAttributes.GENERIC_MOVEMENT_SPEED, AttributeModifierPower.SPEED_MODIFIER, 0.02, (modifierInfo) -> {return modifierInfo.level() * modifierInfo.baseModifierValue();}, EntityAttributeModifier.Operation.ADD_VALUE);
-    public static IncarnaPower CONSTANT_JUMP_STRENGTH_MODIFIER = new AttributeModifierPower(EntityAttributes.GENERIC_JUMP_STRENGTH, AttributeModifierPower.JUMP_STRENGTH_MODIFIER, 0.1, (modifierInfo) -> {return modifierInfo.level() * modifierInfo.baseModifierValue();}, EntityAttributeModifier.Operation.ADD_VALUE);
-    public static IncarnaPower CONSTANT_ARMOR_MODIFIER = new AttributeModifierPower(EntityAttributes.GENERIC_ARMOR, AttributeModifierPower.ARMOR_MODIFIER, 0.5, (modifierInfo) -> {return modifierInfo.level() * modifierInfo.baseModifierValue();}, EntityAttributeModifier.Operation.ADD_VALUE);
+    public static IncarnaPower CONSTANT_SPEED_MODIFIER = new AttributeModifierPower(EntityAttributes.GENERIC_MOVEMENT_SPEED, AttributeModifierPower.SPEED_MODIFIER, 0.02, (modifierInfo) -> {return 0.5F * levelScaler_1_3(modifierInfo.level()) * modifierInfo.baseModifierValue();}, EntityAttributeModifier.Operation.ADD_VALUE);
+    public static IncarnaPower CONSTANT_JUMP_STRENGTH_MODIFIER = new AttributeModifierPower(EntityAttributes.GENERIC_JUMP_STRENGTH, AttributeModifierPower.JUMP_STRENGTH_MODIFIER, 0.1, (modifierInfo) -> {return 0.5F * levelScaler_1_3(modifierInfo.level()) * modifierInfo.baseModifierValue();}, EntityAttributeModifier.Operation.ADD_VALUE);
+    public static IncarnaPower CONSTANT_ARMOR_MODIFIER = new AttributeModifierPower(EntityAttributes.GENERIC_ARMOR, AttributeModifierPower.ARMOR_MODIFIER, 0.5, (modifierInfo) -> {return 0.5F * levelScaler_1_3(modifierInfo.level()) * modifierInfo.baseModifierValue();}, EntityAttributeModifier.Operation.ADD_VALUE);
     public static IncarnaPower CONSTANT_SCALE_MODIFIER = new AttributeModifierPower(EntityAttributes.GENERIC_SCALE, AttributeModifierPower.SCALE_MODIFIER, 0.5, (modifierInfo) -> {return modifierInfo.baseModifierValue();}, EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE);
-    public static IncarnaPower INCREASED_FALL_DAMAGE = new DamageSufferedModifierPower(DamageTypes.FALL, 3.0F, (modifierInfo) -> {return modifierInfo.level() * (float)modifierInfo.baseModifierValue();}, DamageSufferedModifierPower.Operation.MULTIPLY);
+    public static IncarnaPower INCREASED_FALL_DAMAGE = new DamageSufferedModifierPower(DamageTypes.FALL, 3.0F, (modifierInfo) -> {return 0.5F * levelScaler_1_3(modifierInfo.level()) * (float)modifierInfo.baseModifierValue();}, DamageSufferedModifierPower.Operation.MULTIPLY);
     public static IncarnaPower CANCEL_PROJECTILE_DAMAGE = new CancelDamageSufferedPower((damageInfo -> {return damageInfo.damageSource().getSource() instanceof ProjectileEntity;}));
     public static IncarnaPower CANCEL_LOW_FALL_DAMAGE = new CancelDamageSufferedPower((damageInfo -> {return damageInfo.damageSource().isOf(DamageTypes.FALL) && damageInfo.amount() < 6.0F;}));
-    public static IncarnaPower HEAL_IN_WATER = new HealOrDamageOverTimePower(1.0F, (modifierInfo) -> {return modifierInfo.level() * (float)modifierInfo.baseModifierValue();}, (player) -> {return player.isTouchingWater();});
-    public static IncarnaPower DAMAGE_IN_WATER = new HealOrDamageOverTimePower(-1.0F, (modifierInfo) -> {return modifierInfo.level() * (float)modifierInfo.baseModifierValue();}, (player) -> {return player.isTouchingWater();});
-    public static IncarnaPower DAMAGE_IN_NETHER = new HealOrDamageOverTimePower(-1.0F, (modifierInfo) -> {return modifierInfo.level() * (float)modifierInfo.baseModifierValue();}, (player) -> {return player.getWorld().getDimensionEntry().matchesKey(DimensionTypes.THE_NETHER);});
+    public static IncarnaPower HEAL_IN_WATER = new HealOrDamageOverTimePower(1.0F, (modifierInfo) -> {return 0.5F * levelScaler_1_3(modifierInfo.level()) * (float)modifierInfo.baseModifierValue();}, (player) -> {return player.isTouchingWater();});
+    public static IncarnaPower DAMAGE_IN_WATER = new HealOrDamageOverTimePower(-1.0F, (modifierInfo) -> {return 0.5F * levelScaler_1_3(modifierInfo.level()) * (float)modifierInfo.baseModifierValue();}, (player) -> {return player.isTouchingWater();});
+    public static IncarnaPower DAMAGE_IN_NETHER = new HealOrDamageOverTimePower(-1.0F, (modifierInfo) -> {return 0.5F * levelScaler_1_3(modifierInfo.level()) * (float)modifierInfo.baseModifierValue();}, (player) -> {return player.getWorld().getDimensionEntry().matchesKey(DimensionTypes.THE_NETHER);});
     public static IncarnaPower FIRE_RESISTANCE_ABILITY = new StatusEffectAbilityPower((level) -> new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 300, 0));
-    public static IncarnaPower SLOW_FALLING_ABILITY = new StatusEffectAbilityPower((level) -> new StatusEffectInstance(StatusEffects.SLOW_FALLING, 300, Math.max(0, level - 1)));
-    public static IncarnaPower SPEED_ABILITY = new StatusEffectAbilityPower((level) -> new StatusEffectInstance(StatusEffects.SPEED, 300, Math.max(0, level - 1)));
+    public static IncarnaPower SLOW_FALLING_ABILITY = new StatusEffectAbilityPower((level) -> new StatusEffectInstance(StatusEffects.SLOW_FALLING, 300, levelScaler_0_2(level)));
+    public static IncarnaPower SPEED_ABILITY = new StatusEffectAbilityPower((level) -> new StatusEffectInstance(StatusEffects.SPEED, 300, levelScaler_0_2(level)));
     public static IncarnaPower THROW_ENDERPEARL_ABILITY = new ThrowEnderPearlAbilityPower();
     public static IncarnaPower TELEPORT_ON_PROJECTILE_DAMAGE = new TeleportOnProjectileHitOrDeflectPower(projectile -> {return projectile instanceof PersistentProjectileEntity || projectile instanceof ThrownEntity && !(projectile instanceof PotionEntity);});
+
+    public static int levelScaler_1_3(int level)
+    {
+        return levelScaler_0_2(level) + 1;
+    }
+
+    public static int levelScaler_0_2(int level)
+    {
+        if (level < 7) {return 0;}
+        else if (level < 9) {return 1;}
+        else {return 2;}
+    }
 }
